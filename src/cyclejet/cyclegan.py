7e492532d0a3a387b26a553344d180235bed6f55
fdreyer <frederic.dreyer@gmail.com>
2019-04-17 19:00:00 +0100

    removed useless data_loader and all the mnist stuff

--- src/cyclejet/cyclegan.py
+++ src/cyclejet/cyclegan.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division
-import scipy
+import scipy, random
 
 from keras.datasets import mnist
-from keras_contrib.layers.normalization import InstanceNormalization
+from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
@@ -12,37 +12,53 @@ from keras.optimizers import Adam
 import datetime
 import matplotlib.pyplot as plt
 import sys
-from data_loader import DataLoader
+from cyclejet.data_loader_jets import DataLoader
+from glund.models.optimizer import build_optimizer
 import numpy as np
 import os
 
-class CycleGAN():
-    def __init__(self):
-        # Input shape
-        self.img_rows = 128
-        self.img_cols = 128
-        self.channels = 3
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+# TODO: add ZCA preprocessor
 
+class CycleGAN():
+    def __init__(self, hps):
+        # Input shape
+        self.img_rows = hps['npixels']
+        self.img_cols = hps['npixels']
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        #self.img_shape = (self.img_rows, self.img_cols)
+        
         # Configure data loader
-        self.dataset_name = 'apple2orange'
-        self.data_loader = DataLoader(dataset_name=self.dataset_name,
+        self.dataset_name = '%s2%s' % (hps['labelA'], hps['labelB'])
+        self.data_loader = DataLoader(hps['data_path'], hps['labelA'],
+                                      hps['labelB'], hps['nev'], hps['navg'],
                                       img_res=(self.img_rows, self.img_cols))
 
+        self.sampleA = self.data_loader.load_data(domain=hps['labelA'],
+                                                  batch_size=hps['batch_test'],
+                                                  is_testing=True,
+                                                  nev_test=hps['nev_test'])
+        self.sampleB = self.data_loader.load_data(domain=hps['labelB'],
+                                                  batch_size=hps['batch_test'],
+                                                  is_testing=True,
+                                                  nev_test=hps['nev_test'])
 
         # Calculate output shape of D (PatchGAN)
         patch = int(self.img_rows / 2**4)
         self.disc_patch = (patch, patch, 1)
 
         # Number of filters in the first layer of G and D
-        self.gf = 32
-        self.df = 64
+        self.gf = hps['g_filters']
+        self.df = hps['d_filters']
 
         # Loss weights
-        self.lambda_cycle = 10.0                    # Cycle-consistency loss
-        self.lambda_id = 0.1 * self.lambda_cycle    # Identity loss
+        # Cycle-consistency loss
+        self.lambda_cycle = hps['lambda_cycle']
+        # Identity loss
+        self.lambda_id = hps['lambda_id_factor'] * self.lambda_cycle
 
-        optimizer = Adam(0.0002, 0.5)
+        #optimizer = Adam(0.0002, 0.5)
+        optimizer = build_optimizer(hps)
 
         # Build and compile the discriminators
         self.d_A = self.build_discriminator()
@@ -97,6 +113,16 @@ class CycleGAN():
                                             self.lambda_cycle, self.lambda_cycle,
                                             self.lambda_id, self.lambda_id ],
                             optimizer=optimizer)
+
+    def save(self, folder):
+        """Save CycleGAN weights to file"""
+        self.g_AB.save_weights('%s/generatorAB.h5' % folder)
+        self.g_BA.save_weights('%s/generatorBA.h5' % folder)
+
+    def load(self, folder):
+        """Load CycleGAN from input folder"""
+        self.g_AB.load_weights('%s/generatorAB.h5' % folder)
+        self.g_BA.load_weights('%s/generatorBA.h5' % folder)
 
     def build_generator(self):
         """U-Net Generator"""
@@ -158,7 +184,7 @@ class CycleGAN():
 
         return Model(img, validity)
 
-    def train(self, epochs, batch_size=1, sample_interval=50):
+    def train(self, epochs, batch_size=1, sample_interval=None):
 
         start_time = datetime.datetime.now()
 
@@ -214,15 +240,17 @@ class CycleGAN():
                                                                             elapsed_time))
 
                 # If at save interval => save generated image samples
-                if batch_i % sample_interval == 0:
+                if sample_interval and batch_i % sample_interval == 0:
                     self.sample_images(epoch, batch_i)
 
     def sample_images(self, epoch, batch_i):
         os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
-        r, c = 2, 3
+        r, c = 4, 3
 
-        imgs_A = self.data_loader.load_data(domain="A", batch_size=1, is_testing=True)
-        imgs_B = self.data_loader.load_data(domain="B", batch_size=1, is_testing=True)
+        imgs_A = np.array(random.sample(list(self.sampleA),1))
+        imgs_B = np.array(random.sample(list(self.sampleB),1))
+        manyimgs_A = self.sampleA
+        manyimgs_B = self.sampleB
 
         # Demo (for GIF)
         #imgs_A = self.data_loader.load_img('datasets/apple2orange/testA/n07740461_1541.jpg')
@@ -231,29 +259,41 @@ class CycleGAN():
         # Translate images to the other domain
         fake_B = self.g_AB.predict(imgs_A)
         fake_A = self.g_BA.predict(imgs_B)
+        manyfake_B = self.g_AB.predict(manyimgs_A)
+        manyfake_A = self.g_BA.predict(manyimgs_B)
         # Translate back to original domain
         reconstr_A = self.g_BA.predict(fake_B)
         reconstr_B = self.g_AB.predict(fake_A)
+        manyreconstr_A = self.g_BA.predict(manyfake_B)
+        manyreconstr_B = self.g_AB.predict(manyfake_A)
+        gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B,
+                                   np.average(manyimgs_A,axis=0).reshape((1,)+manyimgs_A[0].shape),
+                                   np.average(manyfake_B,axis=0).reshape((1,)+manyfake_B[0].shape),
+                                   np.average(manyreconstr_A,axis=0).reshape((1,)+manyreconstr_A[0].shape),
+                                   np.average(manyimgs_B,axis=0).reshape((1,)+manyimgs_B[0].shape),
+                                   np.average(manyfake_A,axis=0).reshape((1,)+manyfake_A[0].shape),
+                                   np.average(manyreconstr_B,axis=0).reshape((1,)+manyreconstr_B[0].shape)])
 
-        gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B])
-
-        # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
-
-        titles = ['Original', 'Translated', 'Reconstructed']
-        fig, axs = plt.subplots(r, c)
+        titles = ['Translated', 'Reconstructed']
+        fig, axs = plt.subplots(r, c, figsize=(10,10))
         cnt = 0
         for i in range(r):
             for j in range(c):
-                print(gen_imgs.shape)
-                axs[i,j].imshow(gen_imgs[cnt])
-                axs[i, j].set_title(titles[j])
+                axs[i,j].imshow(gen_imgs[cnt][:,:,0],vmin=0.0,vmax=0.8)
+                if j>0:
+                    tit=titles[j-1]
+                elif i==0:
+                    tit='single A sample'
+                elif i==1:
+                    tit='single B sample'
+                elif i==2:
+                    tit='avg A samples'
+                elif i==3:
+                    tit='avg B samples'
+                axs[i, j].set_title(tit)
                 axs[i,j].axis('off')
                 cnt += 1
         fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
         plt.close()
 
 
-if __name__ == '__main__':
-    gan = CycleGAN()
-    gan.train(epochs=200, batch_size=1, sample_interval=200)
