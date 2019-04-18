4cd74350172135d4ebd75d2f44e6159662bac3c8
fdreyer <frederic.dreyer@gmail.com>
2019-04-18 13:50:17 +0100

    changed to 24 pixels

--- src/cyclejet/tools.py
+++ src/cyclejet/tools.py
@@ -19,13 +19,13 @@ def plot_model(fn, refA, refB, predictedA, predictedB):
     """Plot the results for reference and predictions"""
     with PdfPages(fn) as pdf:
         fig, axs = plt.subplots(2,2, figsize=(6,7))
-        axs[0,0].imshow(np.average(refA,axis=0)[:,:],vmin=0.0,vmax=0.7)
+        axs[0,0].imshow(np.average(refA,axis=0)[:,:],vmin=0.0,vmax=0.4)
         axs[0,0].set_title('average A sample')
-        axs[0,1].imshow(np.average(predictedA,axis=0)[:,:],vmin=0.0,vmax=0.7)
+        axs[0,1].imshow(np.average(predictedA,axis=0)[:,:],vmin=0.0,vmax=0.4)
         axs[0,1].set_title('average reconstructed')
-        axs[1,0].imshow(np.average(refB,axis=0)[:,:],vmin=0.0,vmax=0.7)
+        axs[1,0].imshow(np.average(refB,axis=0)[:,:],vmin=0.0,vmax=0.4)
         axs[1,0].set_title('average B sample')
-        axs[1,1].imshow(np.average(predictedB,axis=0)[:,:],vmin=0.0,vmax=0.7)
+        axs[1,1].imshow(np.average(predictedB,axis=0)[:,:],vmin=0.0,vmax=0.4)
         axs[1,1].set_title('average reconstructed')
         plt.close()
         pdf.savefig(fig)
